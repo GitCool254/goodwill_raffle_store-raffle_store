c9c396ed9b55ddbbfc7b74c3b6700f3266149fbe
GitCool254 <goodwillstores35@gmail.com>
2025-12-20 11:08:01 +0300

    debug: log PayPal state before opening

--- src/App.jsx
+++ src/App.jsx
@@ -290,8 +290,8 @@ export default function App() {
           <div
             style={{
               position: "relative",
-              maxWidth: "80vw",
-              maxHeight: "20vh",
+              maxWidth: "60vw",
+              maxHeight: "60vh",
               width: "100%",
               height: "100%",
             }}
