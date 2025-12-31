94ca7cb0b76c621eea15a3893ddad247d21246a1
GitCool254 <goodwillstores35@gmail.com>
2026-01-01 01:33:23 +0300

    debug: log PayPal state before opening

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function App() {
       title: "Coolster 125cc",
       description: "Coolster 125cc.\nGreat little starter for your kid",
       price: 5,
-      image: "/Sauna.jpg",
+      image: "/Big eye polarized sunglass.jpg",
       images: [
         "/Coolster 125cc.png",
         "/Coolster 125cc1.png",
