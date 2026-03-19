9e97765089f4c219b3e016059eee56dde2452a23
GitCool254 <goodwillstores35@gmail.com>
2026-03-20 01:42:32 +0300

    debug: log PayPal state before opening

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -142,8 +142,8 @@ export default function Header({ setView, onMenuClick }) {
                 letterSpacing: '0.01em',
                 color: '#334155',
                 margin: 0,
-                paddingLeft: "15px",
-                paddingRight: "15px",
+                paddingLeft: "10px",
+                paddingRight: "10px",
                 position: 'relative'
               }}
             >
