30737316f5115b8677ec076a46c48bf558c54aab
GitCool254 <goodwillstores35@gmail.com>
2026-04-12 18:12:27 +0300

    debug: log PayPal state before opening

--- src/components/RecentlyViewed.jsx
+++ src/components/RecentlyViewed.jsx
@@ -61,7 +61,7 @@ export default function RecentlyViewed({ onProductClick }) {
                 alt={product.title}
                 style={{
                   width: '100%',
-                  height: '80px',
+                  height: '140px',
                   objectFit: 'contain',
                 }}
               />
