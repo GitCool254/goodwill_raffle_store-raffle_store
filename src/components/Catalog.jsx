a61e7e64782a53e6bd94e0821f99202f74e5363e
GitCool254 <goodwillstores35@gmail.com>
2026-07-23 23:18:06 +0300

    debug: log PayPal state before opening

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -86,10 +86,7 @@ export const catalogItems = [
     title: "4D-V15 Drone Zeros Mini Quadcopter",
     ticketPrice: 5,
     marketPrice: 200,
-    description:
-      "Drone with 1080P camera. Comes with an extra battery, battery
-charging adapter, and propellers restrains.\nThe V15 mini drone captures great in-flight pictures and videos. FPV Real-time transmission, you
-can upload the HD footage from your iphone/ipad.\nComes with instruction manual and accessories.\n\nPerfect working condition.",
+    description: "Drone with 1080P camera. Comes with an extra battery, battery charging adapter, and propellers restrains.\nThe V15 mini drone captures great in-flight pictures and videos. FPV Real-time transmission, you can upload the HD footage from your iphone/ipad.\nComes with instruction manual and accessories.\n\nPerfect working condition.",
     image: "/Drone zeros.png",
     images: [
       "/Drone zeros.png",
